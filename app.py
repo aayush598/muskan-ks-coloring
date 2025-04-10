e90307968279f4d5e8d190d183eecee0e5cb6f2b
aayush598 <aayushgid598@gmail.com>
2025-04-10 16:26:29 +0530

    navbar route updated

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ def init_db():
 def home():
     return render_template('index.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
